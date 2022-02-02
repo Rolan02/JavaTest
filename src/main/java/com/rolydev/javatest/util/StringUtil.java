512bb03f1be619e82754fca81e7c15d154faa7cf
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2022-02-01 21:49:14 -0400

    added test for password

--- src/main/java/com/rolydev/javatest/util/StringUtil.java
+++ src/main/java/com/rolydev/javatest/util/StringUtil.java
@@ -8,4 +8,28 @@ public class StringUtil {
         }
         return  result;
     }
+
+    public static class PasswordUtil {
+        public enum SecurityLevel{
+            WEAK,
+            MEDIUM,
+            STRONG
+        }
+
+        public static SecurityLevel assessPasswordSecurity(String password) {
+            if (password.length() < 8) {
+                return SecurityLevel.WEAK;
+            }
+            if (password.matches("[a-zA-Z]+")) {
+                return  SecurityLevel.WEAK;
+            }
+            if (password.length() < 16) {
+                return SecurityLevel.MEDIUM;
+            }
+            if (password.matches("[a-zA-Z0-9]+")) {
+                return SecurityLevel.STRONG;
+            }
+            return SecurityLevel.STRONG;
+        }
+    }
 }
--- src/test/java/com/rolydev/javatest/util/PasswordUtilTest.java
+++ src/test/java/com/rolydev/javatest/util/PasswordUtilTest.java
@@ -2,14 +2,27 @@ package com.rolydev.javatest.util;
 
 import org.junit.jupiter.api.Test;
 
-import static com.rolydev.javatest.util.PasswordUtil.SecurityLevel.WEAK;
+import static com.rolydev.javatest.util.StringUtil.PasswordUtil.SecurityLevel.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 //import static org.junit.Assert.*;
 
 public class PasswordUtilTest {
 
     @Test
-    public void weak_when_has_than_8_letter() {
-        assertEquals(WEAK,PasswordUtil.assessPasswordSecurity("1234567"));
+    public void weak_when_has_than_8_letters() {
+        assertEquals(WEAK, StringUtil.PasswordUtil.assessPasswordSecurity("1234567"));
     }
+    @Test
+    public void weak_when_has_than_only_letters() {
+        assertEquals(WEAK, StringUtil.PasswordUtil.assessPasswordSecurity("adasdad"));
+    }
+    @Test
+    public void medium_when_has_letters_and_numbers() {
+        assertEquals(MEDIUM, StringUtil.PasswordUtil.assessPasswordSecurity("adasdad123"));
+    }
+    @Test
+    public void strong_when_has_letters_numbers_and_symbols() {
+        assertEquals(STRONG, StringUtil.PasswordUtil.assessPasswordSecurity("ASDasd123!14khvyj"));
+    }
+
 }
